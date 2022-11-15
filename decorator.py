e5c126051da340c7b2961d8ea69f17e2bdea2564
isinka <altarika@icloud.com>
2022-11-15 14:38:11 +0200

    Add function for searching similar commands

--- decorator.py
+++ decorator.py
@@ -16,19 +16,4 @@ def input_error(func):
             return "Вводити потрібно (Ім'я та наступна інформація)"
         except PathError:
             return "Потрібно ввести корректний шлях до папки."
-        # except CommandError:
-        #     command_prefix = ' '.join(command for command in args[0][:args[1]])
-        #     print(f"Wrong command - {command_prefix} {args[0][args[1]]}")
-        #     correct_commands = parse_utils.find_command(args[0][args[1]])
-        #     if correct_commands:
-        #         print('Try:')
-        #         for correct_command in correct_commands:
-        #             print(f"  {command_prefix} {correct_command}")
     return wrapper
-
-
-# def similar_command(func):
-#     def wrapper(*args):
-#         try:
-#             return func(*args)
-#         except KeyError as e:
